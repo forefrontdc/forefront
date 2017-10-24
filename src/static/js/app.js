5aa01766d4a66c89a6d96c83ebae0fc6406ec5be
j-baconator <julie@isl.co>
2017-10-24 13:04:38 -0400

    add logotype svg

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,14 +1,12 @@
-var color = ["#CC99FF", "#FF99CC", "#FF9999", "#FFCC99"]
+const color = ['#CC99FF', '#FF99CC', '#FF9999', '#FFCC99']
 
 function changeColor() {
-  var lastColorIndex = localStorage.getItem('lastColorIndex') || -1
-  var randomColor = -1
+  const lastColorIndex = localStorage.getItem('lastColorIndex') || -1
+  let randomColor = -1
 
   while(lastColorIndex == randomColor || randomColor === -1) {
     randomColor = Math.floor(Math.random() * color.length)
   }
-
-  console.log(color[randomColor])
 
   localStorage.setItem('lastColorIndex',randomColor)
   document.body.style.setProperty('--primary-color', color[randomColor])
